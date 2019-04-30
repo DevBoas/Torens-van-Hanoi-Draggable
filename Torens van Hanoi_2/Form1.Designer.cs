945a3d0f67a4d5129e8fd6336ef1a6ac1397a451
Boas Hoeven <BoasHoeven@gmail.com>
2019-04-30 16:57:19 +0200

    Add project files.

---
+++ Torens van Hanoi_2/Form1.Designer.cs
@@ -0,0 +1,237 @@
+using System.Windows.Forms;
+
+namespace Torens_van_Hanoi_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Ring_7 = new System.Windows.Forms.PictureBox();
+            this.Pole_1 = new System.Windows.Forms.PictureBox();
+            this.Ring_8 = new System.Windows.Forms.PictureBox();
+            this.Ring_6 = new System.Windows.Forms.PictureBox();
+            this.Ring_5 = new System.Windows.Forms.PictureBox();
+            this.Ring_4 = new System.Windows.Forms.PictureBox();
+            this.Ring_3 = new System.Windows.Forms.PictureBox();
+            this.Ring_2 = new System.Windows.Forms.PictureBox();
+            this.Ring_1 = new System.Windows.Forms.PictureBox();
+            this.Pole_2 = new System.Windows.Forms.PictureBox();
+            this.Pole_3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Ring_7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pole_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ring_8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ring_6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ring_5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ring_4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ring_3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ring_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ring_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pole_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pole_3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Ring_7
+            // 
+            this.Ring_7.Image = global::Torens_van_Hanoi_2.Properties.Resources.Ring;
+            this.Ring_7.Location = new System.Drawing.Point(23, 181);
+            this.Ring_7.Name = "Ring_7";
+            this.Ring_7.Size = new System.Drawing.Size(140, 10);
+            this.Ring_7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Ring_7.TabIndex = 1;
+            this.Ring_7.TabStop = false;
+            this.Ring_7.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Ring_MouseDown);
+            this.Ring_7.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Ring_MouseMove);
+            this.Ring_7.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Ring_MouseUp);
+            // 
+            // Pole_1
+            // 
+            this.Pole_1.Image = global::Torens_van_Hanoi_2.Properties.Resources.Pole;
+            this.Pole_1.Location = new System.Drawing.Point(91, 97);
+            this.Pole_1.Name = "Pole_1";
+            this.Pole_1.Size = new System.Drawing.Size(10, 110);
+            this.Pole_1.TabIndex = 0;
+            this.Pole_1.TabStop = false;
+            // 
+            // Ring_8
+            // 
+            this.Ring_8.Image = global::Torens_van_Hanoi_2.Properties.Resources.Ring;
+            this.Ring_8.Location = new System.Drawing.Point(23, 197);
+            this.Ring_8.Name = "Ring_8";
+            this.Ring_8.Size = new System.Drawing.Size(160, 10);
+            this.Ring_8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Ring_8.TabIndex = 2;
+            this.Ring_8.TabStop = false;
+            this.Ring_8.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Ring_MouseDown);
+            this.Ring_8.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Ring_MouseMove);
+            this.Ring_8.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Ring_MouseUp);
+            // 
+            // Ring_6
+            // 
+            this.Ring_6.Image = global::Torens_van_Hanoi_2.Properties.Resources.Ring;
+            this.Ring_6.Location = new System.Drawing.Point(23, 165);
+            this.Ring_6.Name = "Ring_6";
+            this.Ring_6.Size = new System.Drawing.Size(120, 10);
+            this.Ring_6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Ring_6.TabIndex = 3;
+            this.Ring_6.TabStop = false;
+            this.Ring_6.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Ring_MouseDown);
+            this.Ring_6.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Ring_MouseMove);
+            this.Ring_6.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Ring_MouseUp);
+            // 
+            // Ring_5
+            // 
+            this.Ring_5.Image = global::Torens_van_Hanoi_2.Properties.Resources.Ring;
+            this.Ring_5.Location = new System.Drawing.Point(23, 149);
+            this.Ring_5.Name = "Ring_5";
+            this.Ring_5.Size = new System.Drawing.Size(100, 10);
+            this.Ring_5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Ring_5.TabIndex = 4;
+            this.Ring_5.TabStop = false;
+            this.Ring_5.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Ring_MouseDown);
+            this.Ring_5.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Ring_MouseMove);
+            this.Ring_5.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Ring_MouseUp);
+            // 
+            // Ring_4
+            // 
+            this.Ring_4.Image = global::Torens_van_Hanoi_2.Properties.Resources.Ring;
+            this.Ring_4.Location = new System.Drawing.Point(23, 133);
+            this.Ring_4.Name = "Ring_4";
+            this.Ring_4.Size = new System.Drawing.Size(80, 10);
+            this.Ring_4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Ring_4.TabIndex = 5;
+            this.Ring_4.TabStop = false;
+            this.Ring_4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Ring_MouseDown);
+            this.Ring_4.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Ring_MouseMove);
+            this.Ring_4.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Ring_MouseUp);
+            // 
+            // Ring_3
+            // 
+            this.Ring_3.Image = global::Torens_van_Hanoi_2.Properties.Resources.Ring;
+            this.Ring_3.Location = new System.Drawing.Point(23, 117);
+            this.Ring_3.Name = "Ring_3";
+            this.Ring_3.Size = new System.Drawing.Size(60, 10);
+            this.Ring_3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Ring_3.TabIndex = 6;
+            this.Ring_3.TabStop = false;
+            this.Ring_3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Ring_MouseDown);
+            this.Ring_3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Ring_MouseMove);
+            this.Ring_3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Ring_MouseUp);
+            // 
+            // Ring_2
+            // 
+            this.Ring_2.Image = global::Torens_van_Hanoi_2.Properties.Resources.Ring;
+            this.Ring_2.Location = new System.Drawing.Point(23, 101);
+            this.Ring_2.Name = "Ring_2";
+            this.Ring_2.Size = new System.Drawing.Size(40, 10);
+            this.Ring_2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Ring_2.TabIndex = 7;
+            this.Ring_2.TabStop = false;
+            this.Ring_2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Ring_MouseDown);
+            this.Ring_2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Ring_MouseMove);
+            this.Ring_2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Ring_MouseUp);
+            // 
+            // Ring_1
+            // 
+            this.Ring_1.Image = global::Torens_van_Hanoi_2.Properties.Resources.Ring;
+            this.Ring_1.Location = new System.Drawing.Point(23, 85);
+            this.Ring_1.Name = "Ring_1";
+            this.Ring_1.Size = new System.Drawing.Size(20, 10);
+            this.Ring_1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Ring_1.TabIndex = 8;
+            this.Ring_1.TabStop = false;
+            this.Ring_1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Ring_MouseDown);
+            this.Ring_1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Ring_MouseMove);
+            this.Ring_1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Ring_MouseUp);
+            // 
+            // Pole_2
+            // 
+            this.Pole_2.Image = global::Torens_van_Hanoi_2.Properties.Resources.Pole;
+            this.Pole_2.Location = new System.Drawing.Point(341, 97);
+            this.Pole_2.Name = "Pole_2";
+            this.Pole_2.Size = new System.Drawing.Size(10, 110);
+            this.Pole_2.TabIndex = 9;
+            this.Pole_2.TabStop = false;
+            // 
+            // Pole_3
+            // 
+            this.Pole_3.Image = global::Torens_van_Hanoi_2.Properties.Resources.Pole;
+            this.Pole_3.Location = new System.Drawing.Point(591, 97);
+            this.Pole_3.Name = "Pole_3";
+            this.Pole_3.Size = new System.Drawing.Size(10, 110);
+            this.Pole_3.TabIndex = 10;
+            this.Pole_3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 292);
+            this.Controls.Add(this.Ring_1);
+            this.Controls.Add(this.Ring_2);
+            this.Controls.Add(this.Ring_3);
+            this.Controls.Add(this.Ring_7);
+            this.Controls.Add(this.Ring_6);
+            this.Controls.Add(this.Ring_5);
+            this.Controls.Add(this.Ring_4);
+            this.Controls.Add(this.Ring_8);
+            this.Controls.Add(this.Pole_2);
+            this.Controls.Add(this.Pole_1);
+            this.Controls.Add(this.Pole_3);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Ring_7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pole_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ring_8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ring_6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ring_5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ring_4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ring_3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ring_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ring_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pole_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pole_3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Pole_1;
+        private System.Windows.Forms.PictureBox Ring_7;
+        private System.Windows.Forms.PictureBox Ring_8;
+        private System.Windows.Forms.PictureBox Ring_6;
+        private System.Windows.Forms.PictureBox Ring_5;
+        private System.Windows.Forms.PictureBox Ring_4;
+        private System.Windows.Forms.PictureBox Ring_3;
+        private System.Windows.Forms.PictureBox Ring_2;
+        private System.Windows.Forms.PictureBox Ring_1;
+        private System.Windows.Forms.PictureBox Pole_2;
+        private System.Windows.Forms.PictureBox Pole_3;
+    }
+}
+
---
+++ Torens van Hanoi_2/Form1.cs
@@ -0,0 +1,228 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// This is the code for your desktop app.
+// Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+
+namespace Torens_van_Hanoi_2
+{
+    public partial class Form1 : Form
+    {
+        int[][] jaggedArray3 =
+        {
+            new int[] { 0, 0, 0, 0, 0, 0, 0, 0}, // tower 1
+            new int[] { 0, 0, 0, 0, 0, 0, 0, 0 }, // tower 2
+            new int[] { 0, 0, 0, 0, 0, 0, 0, 0 }, // tower 3
+        };
+        public Form1()
+        {
+            InitializeComponent();
+            AddRing(Pole_1, Ring_8);
+            AddRing(Pole_1, Ring_7);
+            AddRing(Pole_1, Ring_6);
+            AddRing(Pole_1, Ring_5);
+            AddRing(Pole_1, Ring_4);
+            AddRing(Pole_1, Ring_3);
+            AddRing(Pole_1, Ring_2);
+            AddRing(Pole_1, Ring_1);
+        }
+
+        private int getOffset(PictureBox pole, PictureBox ring)
+        {
+            int c = 0;
+            int PoleNum = System.Convert.ToInt32(pole.Name.Substring(pole.Name.Length-1));
+            int RingNum = System.Convert.ToInt32(ring.Name.Substring(ring.Name.Length-1));
+            for (int i = 0; i < jaggedArray3[PoleNum - 1].Length; i++)
+            {
+                //MessageBox.Show(RingNum.ToString() + " < " + jaggedArray3[PoleNum - 1][i].ToString());
+                if (RingNum < jaggedArray3[PoleNum - 1][i] || jaggedArray3[PoleNum - 1][i] == 0)
+                {
+                    if (jaggedArray3[PoleNum - 1][i] > 0)
+                        c++;
+                    else
+                    {
+                        jaggedArray3[PoleNum - 1][i] = RingNum;
+                        break;
+                    }
+                }
+                else
+                {
+                    ///MessageBox.Show("No can do");
+                    return -1;
+                }
+            }
+            return c * ring.Height;
+        }
+
+        private Control activeControl;
+        private Point previousLocation;
+        private Point PickupPoint;
+
+        private Boolean CanPickUp(Control con)
+        {
+            PictureBox ring = con as PictureBox;
+            int RingNum = System.Convert.ToInt32(ring.Name.Substring(ring.Name.Length - 1));
+            //MessageBox.Show("Attempting to pickup ring" + RingNum.ToString());
+            int found = -1;
+            for (int i = 0; i < jaggedArray3.Length; i++)
+            {
+                for (int y = 0; y < jaggedArray3[i].Length; y++)
+                {
+                    if ((found == i) && (jaggedArray3[i][y] > 0))
+                        return false;
+                    if (jaggedArray3[i][y] == RingNum)
+                        found = i;
+                }
+            }
+            return true;
+        }
+
+        void Ring_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (!CanPickUp(sender as Control))
+                return;
+            activeControl = sender as Control;
+            PickupPoint = activeControl.Location;
+            previousLocation = e.Location;
+            //Cursor = Cursors.Hand;
+        }
+
+        private PictureBox matchPole(int num)
+        {
+            PictureBox pole = null;
+            foreach (Control c in Controls)
+            {
+                if (c.GetType() == typeof(PictureBox) && c.Name.Substring(0, 5) == "Pole_")
+                {
+                    int PoleNum = System.Convert.ToInt32(c.Name.Substring(c.Name.Length - 1));
+                    if (PoleNum == num)
+                        pole = c as PictureBox;
+                }
+            }
+            return pole;
+        }
+
+        private Boolean checkInSquare(Control activeControl)
+        {
+            Point a = activeControl.Location;
+            PictureBox ring = activeControl as PictureBox;
+
+            int p1_x_l_top = Pole_1.Location.X - 125 - (activeControl.Size.Width / 2);
+            int p1_x_r_top = Pole_1.Location.X + 125 + (activeControl.Size.Width / 2); 
+            int p1_y_top = Pole_1.Location.Y - 50;
+            int p1_y_bot = Pole_1.Location.Y + Pole_1.Size.Height;
+
+            int p2_x_l_top = Pole_2.Location.X - 125 - (activeControl.Size.Width / 2);
+            int p2_x_r_top = Pole_2.Location.X + 125 + (activeControl.Size.Width / 2);
+            int p2_y_top = Pole_2.Location.Y - 50;
+            int p2_y_bot = Pole_2.Location.Y + Pole_1.Size.Height;
+
+            int p3_x_l_top = Pole_3.Location.X - 125 - (activeControl.Size.Width / 2);
+            int p3_x_r_top = Pole_3.Location.X + 125 + (activeControl.Size.Width / 2);
+            int p3_y_top = Pole_3.Location.Y - 50;
+            int p3_y_bot = Pole_2.Location.Y + Pole_1.Size.Height;
+
+            Boolean inSquare = false;
+            //MessageBox.Show(a.X.ToString() + " > " + x_l_top.ToString());
+            //MessageBox.Show(a.X.ToString() + " < " + x_r_top.ToString());
+            //MessageBox.Show(a.Y.ToString() + " < " + y_top.ToString());
+
+            // Pole 1 square
+            if ((a.X >= p1_x_l_top) && (a.X <= p1_x_r_top) && (a.Y >= p1_y_top) && a.Y <= p1_y_bot)
+            {
+                ///MessageBox.Show("Drag is in dropbox of pole_1");
+                int poleInt = RemoveRing(ring);
+                PictureBox pole = matchPole(poleInt);
+                Boolean possible = AddRing(Pole_1, ring);
+                if (possible)
+                    inSquare = true;
+                else
+                    AddRing(pole, ring);
+            }
+
+            // Pole 2 square
+            if ((a.X >= p2_x_l_top) && (a.X <= p2_x_r_top) && (a.Y >= p2_y_top) && a.Y <= p2_y_bot)
+            {
+                ///MessageBox.Show("Drag is in dropbox of pole_2");
+                //RemoveRing(ring);
+                int poleInt = RemoveRing(ring);
+                PictureBox pole = matchPole(poleInt);
+                Boolean possible = AddRing(Pole_2, ring);
+                if (possible)
+                    inSquare = true;
+                else
+                    AddRing(pole, ring);
+            }
+
+            // Pole 3 square
+            if ((a.X >= p3_x_l_top) && (a.X <= p3_x_r_top) && (a.Y >= p3_y_top) && a.Y <= p3_y_bot)
+            {
+                ///MessageBox.Show("Drag is in dropbox of pole_3");
+                int poleInt = RemoveRing(ring);
+                PictureBox pole = matchPole(poleInt);
+                Boolean possible = AddRing(Pole_3, ring);
+                if (possible)
+                    inSquare = true;
+                else
+                    AddRing(pole, ring);
+            }
+
+            return inSquare;
+        }
+        void Ring_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (activeControl != null && !checkInSquare(activeControl))
+                activeControl.Location = PickupPoint;
+            activeControl = null;
+            //Cursor = Cursors.Default;
+            //MessageBox.Show("Check in region");
+        }
+
+        void Ring_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (activeControl == null || activeControl != sender)
+                return;
+
+            var location = activeControl.Location;
+            location.Offset(e.Location.X - previousLocation.X, e.Location.Y - previousLocation.Y);
+            activeControl.Location = location;
+        }
+
+        private int RemoveRing(PictureBox ring)
+        {
+            int RingNum = System.Convert.ToInt32(ring.Name.Substring(ring.Name.Length - 1));
+            for (int i = 0; i < jaggedArray3.Length; i++)
+            {
+                for (int y = 0; y < jaggedArray3[i].Length; y++)
+                {
+                    if (jaggedArray3[i][y] == RingNum)
+                    {
+                        jaggedArray3[i][y] = 0;
+                        return i+1;
+                    }
+                }
+            }
+            return -1;
+        }
+
+        private Boolean AddRing(PictureBox pole, PictureBox ring)
+        {
+            int offset = getOffset(pole, ring);
+            if (offset == -1)
+                return false;
+            int y_position = pole.Location.Y - offset;
+            int y_size = pole.Size.Height - ring.Size.Height;
+            int x_position = pole.Location.X;
+            int x_size = (ring.Size.Width/2) - (pole.Size.Width / 2);
+            ring.Location = new Point((x_position - x_size) , y_position + y_size);
+            return true;
+        }
+    }
+}
